0fe57f0fae30df8979ab47efc3f400a78479dc71
unknown <kacpercele@gmail.com>
2023-12-10 23:03:21 +0100

    feat/Create home view

---
+++ vite-project/src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="w-1/2 bg-black h-screen">
+ <h1 class="text-left text-[#FFFF] font-['Work_Sans']">Recommendation-Hub</h1>
+       <router-link to="/companies" class="text-[#FFFF] font-['Work_Sans']">Companies</router-link>
+    </div>
+</template>
